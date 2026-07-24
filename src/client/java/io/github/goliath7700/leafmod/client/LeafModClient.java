66a64cd927e104a4e93c71d818267987fe5a6fda
Goliath7700 <cherryplant843@gmail.com>
2026-07-23 22:38:44 -0700

    Initial commit + MIT License

---
+++ src/client/java/io/github/goliath7700/leafmod/client/LeafModClient.java
@@ -0,0 +1,10 @@
+package io.github.goliath7700.leafmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LeafModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/goliath7700/leafmod/client/LeafModDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.goliath7700.leafmod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LeafModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
